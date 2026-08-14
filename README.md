2454a187cc5bc6fdb0b3e0b018ad2547041c74ae
manish <m.kumar7411@gmail.com>
2026-08-14 21:14:04 +0530

    first setup commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Created project using Vite config.
+ - Created Project using "npm create vite@latest".
+ - Installed TailwindCss using vite config.
+
+# Features to built
+ - Before login page features:
+    > Login page
+    > Trending now section with horizontal scroll
+    > FAQ section
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     Netflix clone
+     <h1 class="text-3xl font-bold underline text-red-500">
+    Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default App
